a67640caf9a6a2cc4814bc6ede0b82cf7ecb4db3
이정헌 <aoc05230@naver.com>
2025-08-02 11:07:05 +0900

    First commit

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        super.service(req, resp);
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8"); // 인코딩 정보를 알려줌, Content-Type 헤더 정보에 들어감.
+        response.getWriter().write("Hello " + username + "!"); // HTTP 메시지 바디에 write한 내용이 들어감.
+    }
+}
