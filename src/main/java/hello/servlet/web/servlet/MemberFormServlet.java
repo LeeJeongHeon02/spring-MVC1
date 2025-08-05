792498f4d0975b12e565829e104c3215074f896b
이정헌 <aoc05230@naver.com>
2025-08-06 05:16:35 +0900

    ~25 lecture finish

---
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,48 @@
+package hello.servlet.web.servlet;
+
+
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // ContentType은 text/html로
+        response.setContentType("text/html");
+
+        // Encoding은 UTF-8
+        response.setCharacterEncoding("UTF-8");
+
+        /*
+        * getWriter을 이용해서 HTML 적기... 하나하나 치기 힘드니 복붙 ㄱ
+        * */
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    유저네임: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                "    <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
